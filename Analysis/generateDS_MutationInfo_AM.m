764a55bbed32927406fad4a0ce82a7a9f4b0670f
Alsu Missarova <amissarova@Xiaoyuns-MacBook-Air-2.local>
2018-07-27 18:23:40 +0200

    w conservation score

--- Analysis/generateDS_MutationInfo_AM.m
+++ Analysis/generateDS_MutationInfo_AM.m
@@ -2,11 +2,11 @@ cd ~/Develop/Phix_mutagenesis/
 addpath(genpath('~/Develop/matlab'));
 
 %% Pipeline to generate DS w/ all possible mutations and their annotations
-
 % read fasta file with whole sequence
 PhixGenome = readSequenceFromFasta('~/Develop/Phix_mutagenesis/ExternalData/genomeNC_001422.1.fasta');
+PhixGenome2x = strcat(PhixGenome , PhixGenome);
 
-%% make a DS w all possible combinations
+% make a DS w all possible combinations
 nucleotides_cell = {'A' , 'C' , 'G' , 'T'};
 nucleotides_sub_cell = cell( length(nucleotides_cell) , length(nucleotides_cell) - 1);
 for I = 1:length(nucleotides_cell)
@@ -38,7 +38,7 @@ end
 save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS');      
         
 % add info reagarding a nucleotide being a common SNP or ARS
-anno = dataset('file','~/Develop/Phix_mutagenesis/ExternalData/annotation.tab');
+anno = dataset('file','~/Develop/Phix_mutagenesis/ExternalData/annotationNC_001422.1.tab');
 idx = find(~strcmp(anno.Type , 'CDS') & ~strcmp(anno.Type , 'mRNA/exon'));
 anno = anno(idx , :);
 DS.SpecialFeatures = cell(length(DS) , 1);        
@@ -48,34 +48,76 @@ for I = 1:length(DS)
         DS.SpecialFeatures{I} = anno.Type{idx};    
     end
 end
-
+%
 % add info regarding to which annotated genes position belongs
-anno = dataset('file','~/Develop/Phix_mutagenesis/ExternalData/annotation.tab');
+anno = dataset('file','~/Develop/Phix_mutagenesis/ExternalData/annotationNC_001422.1.tab');
+AA_table = dataset('file' , '~/Develop/Phix_mutagenesis/ExternalData/AA_table.tab');
 idx = find(strcmp(anno.Type , 'CDS') );
 anno = anno(idx , :);
 DS.GeneBounds = cell(length(DS) , 1);
 DS.GeneAnnotations = cell(length(DS) , 1);
 DS.PositionInGene = cell(length(DS) , 1);
+
+DS.AA = cell(length(DS) , 1);
+DS.PositionInProtein = cell(length(DS) , 1);
+DS.AASubstitutes = cell(length(DS) , 1);
+
 N = max(DS.PositionNum);
 for I = 1:length(DS)
+    
     temp_position = DS.PositionNum(I);
     temp_GeneBounds = [];
     temp_GeneAnnotations = cell(0);
+    temp_AA = cell(0);
+    temp_AA_alt = cell(0);
     temp_PositionInGene = [];
+    temp_PositionInProtein = [];
     for J = 1:length(anno)
         if temp_position >= anno.PositionStart(J) & temp_position <= anno.PositionEnd(J)
             temp_GeneBounds = [temp_GeneBounds; anno.PositionStart(J) anno.PositionEnd(J)];
-            K = length(temp_GeneAnnotations); temp_GeneAnnotations{K+1} = anno.Type{J};
+            K = length(temp_GeneAnnotations); 
+            temp_GeneAnnotations{K+1} = anno.Name{J};
             temp_PositionInGene = [temp_PositionInGene; temp_position - anno.PositionStart(J) + 1];
+            
+            gene = '';
+            for Z = anno.PositionStart(J):anno.PositionEnd(J)
+                gene = strcat(gene , PhixGenome2x(Z));
+            end
+            [codon , codon_num] = getCodonFromGenePosition(DS.PositionNucleotide{I} , temp_position - anno.PositionStart(J) + 1 , gene);
+            [codon_alt] = getCodonFromGenePosition(DS.PositionNucleotideSubstitute{I} , temp_position - anno.PositionStart(J) + 1 , gene);
+            aa = getAAfromCodon(codon , AA_table);
+            aa_alt = getAAfromCodon(codon_alt , AA_table);
+            temp_AA{K+1} = aa;
+            temp_AA_alt{K+1} = aa_alt;
+            temp_PositionInProtein = [temp_PositionInProtein ; codon_num];
+            
         elseif anno.PositionEnd(J) > N & temp_position <= anno.PositionEnd(J) - N
             temp_GeneBounds = [temp_GeneBounds; anno.PositionStart(J) anno.PositionEnd(J)];
-            K = length(temp_GeneAnnotations); temp_GeneAnnotations{K+1} = anno.Type{J};
+            K = length(temp_GeneAnnotations); 
+            temp_GeneAnnotations{K+1} = anno.Name{J};
             temp_PositionInGene = [temp_PositionInGene; temp_position + N - anno.PositionStart(J) + 1];
+            
+            gene = '';
+            gene = '';
+            for Z = anno.PositionStart(J):anno.PositionEnd(J)
+                gene = strcat(gene , PhixGenome2x(Z));
+            end
+            [codon , codon_num] = getCodonFromGenePosition(DS.PositionNucleotide{I} , temp_position + N - anno.PositionStart(J) + 1 , gene);
+            [codon_alt] = getCodonFromGenePosition(DS.PositionNucleotideSubstitute{I} , temp_position + N - anno.PositionStart(J) + 1 , gene);
+            aa = getAAfromCodon(codon , AA_table);
+            aa_alt = getAAfromCodon(codon_alt , AA_table);
+            temp_AA{K+1} = aa;
+            temp_AA_alt{K+1} = aa_alt;
+            temp_PositionInProtein = [temp_PositionInProtein ; codon_num];
+            
         end
     end
     DS.GeneBounds{I} = temp_GeneBounds;
     DS.GeneAnnotations{I} = temp_GeneAnnotations;
     DS.PositionInGene{I} = temp_PositionInGene;
+    DS.AA{I} = temp_AA;
+    DS.AASubstitutes{I} = temp_AA_alt;
+    DS.PositionInProtein{I} = temp_PositionInProtein;
 end
 save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS');    
 
@@ -110,7 +152,7 @@ for I = 1:length(DS)
     end
 end
 save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS');      
-%%
+%
 DS.SynonymousTotalBool = ones(length(DS) , 1);
 for I = 1:length(DS)
     temp_SynonymousBool = DS.SynonymousBool{I};
@@ -144,7 +186,7 @@ N = length(PhixGenome);
 DS.NonsenseBool = cell(length(DS) , 1);
 for I = 1:length(DS)
     
-	fprintf('Processed %.2f.\n' , I/length(DS));
+	%fprintf('Processed %.2f.\n' , I/length(DS));
     
     PositionInGene = DS.PositionInGene{I};
     GeneBounds = DS.GeneBounds{I};
@@ -179,6 +221,52 @@ for I = 1:length(DS)
     end
 end
 save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS');   
+
+%% add a score for conservation
+load('~/Develop/Phix_Mutagenesis/Data/DS_PhylogeneticInfo.mat' );   
+% add vector of alternatives
+
+DS.ConservationScore = cell(length(DS) , 1);
+for I = 1:length(DS)
+    
+    if DS.SynonymousTotalBool(I) == 1
+        DS.ConservationScore{I} = 'S';
+    else
+        AA = DS.AA{I};
+        AASubstitutes = DS.AASubstitutes{I};
+        PositionInProtein = DS.PositionInProtein{I};
+        Genes = DS.GeneAnnotations{I};
+        K = length(AA);
+        count = 0;
+        for J = 1:K
+            if strcmp(AA{J} , AASubstitutes{J})
+                count = count + 1;
+                continue
+            else 
+                idx = find(strcmp(D.Gene , Genes{J}) & D.AAPosition == PositionInProtein(J));
+                if isempty(idx) & strcmp(AA{J} , 'STOP') & ~strcmp(AASubstitutes{J} , 'STOP')
+                    DS.ConservationScore{I} = 'N';
+                    break
+                elseif ~isempty(find(strcmp(D.AASpectrum{idx} , '-')))
+                    DS.ConservationScore{I} = 'X';
+                    break
+                elseif isempty(find(strcmp(D.AASpectrum{idx} , AASubstitutes{J})))
+                    DS.ConservationScore{I} = 'N';
+                    break
+                else
+                    count = count + 1;
+                    continue
+                end
+            end
+            
+        end
+        if count == K
+            DS.ConservationScore{I} = 'C';
+        end
+    end
+end
+save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS'); 
+
 
 %%
 load('~/Develop/Phix_mutagenesis/Data/DS_MutationsInfo.mat');
