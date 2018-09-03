624b2184464cd790db1dacbb524af583e55de06f
Alsu Missarova <amissarova@Xiaoyuns-MacBook-Air-2.local>
2018-09-03 21:52:58 +0200

    current DS to work wit

--- Analysis/generateDS_MutationInfo_AM.m
+++ Analysis/generateDS_MutationInfo_AM.m
@@ -222,9 +222,10 @@ for I = 1:length(DS)
 end
 save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS');   
 
-%% add a score for conservation
-load('~/Develop/Phix_Mutagenesis/Data/DS_PhylogeneticInfo.mat' );   
-% add vector of alternatives
+% add a score for conservation
+load('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat'); 
+load('~/Develop/Phix_Mutagenesis/Data/DS_PhylogeneticInfo.mat');   
+%% add vector of alternatives
 
 DS.ConservationScore = cell(length(DS) , 1);
 for I = 1:length(DS)
@@ -240,34 +241,91 @@ for I = 1:length(DS)
         Genes = DS.GeneAnnotations{I};
         K = length(AA);
         count = 0;
+        count_X = 0;
         for J = 1:K
             if strcmp(AA{J} , AASubstitutes{J})
                 count = count + 1;
-                continue
+                
+                %continue
             else 
                 idx = find(strcmp(D.Gene , Genes{J}) & D.AAPosition == PositionInProtein(J));
                 if isempty(idx) & strcmp(AA{J} , 'STOP') & ~strcmp(AASubstitutes{J} , 'STOP')
-                    DS.ConservationScore{I} = 'Rest';
+                    DS.ConservationScore{I} = 'Nonstop';
+                    
                     break
                 elseif ~isempty(find(strcmp(D.AASpectrum{idx} , '-')))
-                    DS.ConservationScore{I} = 'X';
-                    break
+                    count_X = count_X+1;
+                    continue
                 elseif isempty(find(strcmp(D.AASpectrum{idx} , AASubstitutes{J})))
-                    DS.ConservationScore{I} = 'Rest';
-                    break
+                    continue
                 else
                     count = count + 1;
                     continue
                 end
             end
-            
         end
-        if count == K
-            DS.ConservationScore{I} = 'Conserved';
+        if isempty(DS.ConservationScore{I})
+            if count_X > 0
+                DS.ConservationScore{I} = 'X';
+            elseif count == K
+                DS.ConservationScore{I} = 'Conserved';
+            else
+                DS.ConservationScore{I} = 'Rest';
+            end
         end
     end
 end
 save('~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.mat' , 'DS'); 
+
+
+%% 2. DS w lane info
+load('~/Develop/Phix_mutagenesis/Data/DS_MutationsInfo.mat');
+D = DS(: , {'PositionNum' , 'PositionNucleotide' , 'PositionNucleotideSubstitute'});
+samples = dir('~/Develop/Phix_mutagenesis/Data/lanes'); samples = samples(4:end);
+cd lanes
+for I = 1:length(samples)
+    T = dataset('file' , samples(I).name);
+    T.Properties.VarNames{2} = 'PositionNum';
+    D.Freq = NaN(length(D) , 1);
+    for J = 1:length(D)
+        idx = find(D.PositionNum(J) == T.PositionNum);
+        names_vars_T = get(T , 'VarNames');
+        idx_substitute_T = find(strcmp(D.PositionNucleotideSubstitute{J} , names_vars_T));
+        if T.cov(idx) > 50
+            D.Freq(J) = double(T(idx , idx_substitute_T))/T.cov(idx);
+        end
+    end
+    N = size(D,2);
+    D.Properties.VarNames{N} = sprintf('Frequency_%d' , I);
+end
+cd ..
+
+%
+names_vars_D = get(D , 'VarNames');
+idx = [];
+for I = 1:length(names_vars_D)
+    if findstr('Frequency' , names_vars_D{I})
+        idx = [idx I];
+    end
+end
+D.vecFrequency = cell(length(D) , 1);
+D.meanFrequency = NaN(length(D) , 1);
+D.medianFrequency = NaN(length(D) , 1);
+D.stdFrequency = NaN(length(D) , 1);
+for I = 1:length(D)
+    vecFrequency = NaN(length(idx) , 1);
+    for J = 1:length(idx)
+        vecFrequency(J) = D(I , idx(J));
+    end
+    D.vecFrequency{I} = vecFrequency;
+    D.meanFrequency(I) = nanmean(vecFrequency);
+    D.medianFrequency(I) = nanmedian(vecFrequency);
+    D.stdFrequency(I) = nanstd(vecFrequency);
+end
+D = D(: , {'PositionNum' , 'PositionNucleotide' , 'PositionNucleotideSubstitute' , ...
+    'vecFrequency' , 'meanFrequency' , 'medianFrequency' , 'stdFrequency'});
+save('~/Develop/Phix_Mutagenesis/Data/DS_LanesInfo.mat' , 'D');
+
 
 
 %%
@@ -277,8 +335,44 @@ D = DS(: , {'PositionNum', 'PositionNucleotide' , 'PositionNucleotideSubstitute'
     'NonsenseTotalBool' , 'ConservationScore'});
 export(D , 'file' , '~/Develop/Phix_Mutagenesis/Data/DS_MutationsInfo.tab');
 
+%% add DM for mutations for each substitution
 
+load('~/Develop/Phix_Mutagenesis/Data/DS_LanesInfo.mat');
+D = sortrows(D , 'PositionNum');
+D.DM_from_mode_eachSubstitution = NaN(length(D) , 1);
+D.mode_eachSubstitution = NaN(length(D) , 1);
+nucleotide_original = {'A' , 'C' , 'T' , 'G'};    
+nucleotide_substitute = {'A' , 'C' , 'T' , 'G'};
 
+for J1 = 1:length(nucleotide_original)
+    for J2 = 1:length(nucleotide_substitute)
+        if J1 ~= J2
+            aa = [J1 J2]
+            idx = find(strcmp( D.PositionNucleotide , nucleotide_original{J1}) & ...
+                strcmp(D.PositionNucleotideSubstitute , nucleotide_substitute{J2}) );
+            data = D.medianFrequency(idx);
+            m = modefit( data , 0 , [0:0.0000005:0.0003] ) ;
+            for Z = 1:length(idx)
+                D.mode_eachSubstitution(idx(Z)) = m;
+                D.DM_from_mode_eachSubstitution(idx(Z)) = data(Z) - m;
+            end
+        end
+    end
+end
+save('~/Develop/Phix_Mutagenesis/Data/DS_LanesInfo.mat' , 'D');
+
+%%
+str_NCBI = readSequenceFromFasta('~/Develop/Phix_Mutagenesis/ExternalData/PhiX_NCBI.txt');
+str_Ilumina = readSequenceFromFasta('~/Develop/Phix_Mutagenesis/ExternalData/PhiX_Ilumina.txt');
+N = 5386;
+for I =1:N
+    if ~strcmp(str_NCBI(I) , str_Ilumina(I))
+        fprintf('%d: NCBI -- %s , Ilumina -- %s\n' , I , str_NCBI(I) , str_Ilumina(I));
+    end
+end
+    
+    
+    
 
 
 
